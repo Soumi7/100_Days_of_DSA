bd79670e6959a049bd1c2b52f95f91323db87ab9
Soumi Bardhan <51290447+Soumi7@users.noreply.github.com>
2021-03-18 23:30:53 +0530

    Create Partition_equal_subset_sum_topdown.py

---
+++ Day4/Partition_equal_subset_sum_topdown.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        k=sum(nums)
+        if k%2!=0:
+            return False
+        else:
+            
+            
+            def subsetsum(arr,n,w):
+                
+                for i in range(0,w+1):
+                    dp[0][i] = False
+                    
+                for j in range(0,n+1):
+                    dp[j][0] = True
+                    
+                #print(dp)
+                
+                for i in range(1,n+1):
+                    for j in range(0,w+1):
+                        if arr[i-1] <= j:
+                            dp[i][j] = dp[i-1][j-arr[i-1]] or dp[i-1][j]
+                        else:
+                            dp[i][j] = dp[i-1][j]
+                return dp[n][w]
+                    
+            
+            dp = [  [-1]  *  (  (k//2)   +1  )  ]*(len(nums)+1)
+                    
+            return subsetsum(nums,len(nums),k//2)
+        
+        
+        
