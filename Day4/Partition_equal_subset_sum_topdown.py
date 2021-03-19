69f640b03ebc69febe89e151a057b04c3a382c0c
Soumi Bardhan <51290447+Soumi7@users.noreply.github.com>
2021-03-19 11:48:21 +0530

    Update Partition_equal_subset_sum_topdown.py

--- Day4/Partition_equal_subset_sum_topdown.py
+++ Day4/Partition_equal_subset_sum_topdown.py
@@ -3,31 +3,20 @@ class Solution:
         k=sum(nums)
         if k%2!=0:
             return False
-        else:
-            
-            
-            def subsetsum(arr,n,w):
-                
+        else:         
+            def subsetsum(arr,n,w):                
                 for i in range(0,w+1):
-                    dp[0][i] = False
-                    
+                    dp[0][i] = False                    
                 for j in range(0,n+1):
-                    dp[j][0] = True
-                    
-                #print(dp)
-                
+                    dp[j][0] = True                                
                 for i in range(1,n+1):
                     for j in range(0,w+1):
                         if arr[i-1] <= j:
-                            dp[i][j] = dp[i-1][j-arr[i-1]] or dp[i-1][j]
+                            dp[i][j] = (dp[i-1][j-arr[i-1]]) or (dp[i-1][j])
                         else:
-                            dp[i][j] = dp[i-1][j]
+                            dp[i][j] = dp[i-1][j]                            
+                        #print(i,j)
+                        #print(dp)                                        
                 return dp[n][w]
-                    
-            
-            dp = [  [-1]  *  (  (k//2)   +1  )  ]*(len(nums)+1)
-                    
+            dp = [  [False]  *  (  (k//2)   +1  ) for i in range(0,len(nums)+1) ]                    
             return subsetsum(nums,len(nums),k//2)
-        
-        
-        
