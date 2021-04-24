9ada3bab7f395c1446b8262711b1b3438f5b3ada
Soumi Bardhan <51290447+Soumi7@users.noreply.github.com>
2021-04-24 18:07:53 +0530

    Update Scramble_string_mcm_memoized.py

--- Day12/Scramble_string_mcm_memoized.py
+++ Day12/Scramble_string_mcm_memoized.py
@@ -1,29 +1,32 @@
 class Solution:
+    
     def isScramble(self, s1: str, s2: str) -> bool:
         dic = {}
+        def isScramble1(s1: str, s2: str):
         
-        key = s1 + " " + s2
-        if key in dic:
-            return dic[key]
-        
-        n1 = len(s1)
-        n2 = len(s2)
-        
-        if s1==s2:
-            return True
-        if len(s1)<=1:
-            return False
-        if n1==0 or n2==0:
-            return False      
-        
-        
-        flag = False
-        
-        for i in range(1,min(n1,n2)):
-            #print(s1,s2)
-            
-            if (self.isScramble(s1[0:i],s2[n2-i:n2]) and self.isScramble(s1[i:n1],s2[0:n2-i])) or (self.isScramble(s1[0:i],s2[0:i]) and self.isScramble(s1[i:n1],s2[i:n2])):
-                flag = True
-                break
-        dic[key] = flag
-        return flag
+            key = s1 + " " + s2
+            if key in dic:
+                return dic[key]
+
+            n1 = len(s1)
+            n2 = len(s2)
+
+            if s1==s2:
+                return True
+            if len(s1)<=1:
+                return False
+            if n1==0 or n2==0:
+                return False      
+
+
+            flag = False
+
+            for i in range(1,min(n1,n2)):
+                #print(s1,s2)
+
+                if (isScramble1(s1[0:i],s2[n2-i:n2]) and isScramble1(s1[i:n1],s2[0:n2-i])) or (isScramble1(s1[0:i],s2[0:i]) and isScramble1(s1[i:n1],s2[i:n2])):
+                    flag = True
+                    break
+            dic[key] = flag
+            return flag
+        return isScramble1(s1, s2)
